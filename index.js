0dc82d8130447c529b3bdd561233cbde233abeb2
thashreef <chthashreef22@gmail.com>
2024-08-11 12:02:10 +0400

    media query update

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const sections = document.querySelectorAll("section[id]");
+const button = document.getElementById('ham_button');
 
-function scrollTracker() {
-  const currentYScroll = window.scrollY;
+// function scrollTracker() {
+//   const currentYScroll = window.scrollY;
 
-  sections.forEach((section) => {
-    const sectionHeight = section.offsetHeight;
-    const sectionTop = section.offsetTop - 100; // Adjust for header height if needed
-    const id = section.getAttribute("id");
-    const currentNavLink = document.querySelector(`nav a[href*="#${id}"]`);
-    if (
-      currentYScroll > sectionTop &&
-      currentYScroll <= sectionTop + sectionHeight
-    ) {
-      currentNavLink.classList.add("active");
-    } else {
-      currentNavLink.classList.remove("active");
-    }
-  });
-}
+//   sections.forEach((section) => {
+//     const sectionHeight = section.offsetHeight;
+//     const sectionTop = section.offsetTop - 100; // Adjust for header height if needed
+//     const id = section.getAttribute("id");
+//     const currentNavLink = document.querySelector(`nav a[href*="#${id}"]`);
+//     if (
+//       currentYScroll > sectionTop &&
+//       currentYScroll <= sectionTop + sectionHeight
+//     ) {
+//       currentNavLink.classList.add("active");
+//     } else {
+//       currentNavLink.classList.remove("active");
+//     }
+//   });
+// }
 
-window.addEventListener("scroll", scrollTracker);
+document.querySelector('.menu-button').addEventListener('click', function() {
+  document.querySelector('header').classList.toggle('menu-active');
+});
+
+
+// window.addEventListener("scroll", scrollTracker);
